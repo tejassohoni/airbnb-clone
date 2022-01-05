7be01571ec0993a969b6620c155ec8014095b83b
Tejas Sohoni <tjs1597@gmail.com>
2022-01-05 14:20:57 +0530

    Added next.config.js and the header component

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
+
+export default function Header() {
+  return (
+    <div>
+      <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+        <div className="relative flex items-center h-10 cursor-pointer my-auto">
+          <Image
+            src="https://links.papareact.com/qd3"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="left"
+          />
+        </div>
+        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+          <input
+            type="text"
+            placeholder="Start Your Search"
+            className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          />
+          <SearchIcon
+            className="hidden md:inline-flex h-8 bg-red-400 
+          text-white rounded-full p-2 cursor-pointer md:mx-2"
+          />
+        </div>
+        <div>right</div>
+      </header>
+    </div>
+  );
+}
