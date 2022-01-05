07fc445f150207ca989713fdec9595d1090cc73c
Tejas Sohoni <tjs1597@gmail.com>
2022-01-05 14:51:35 +0530

    Stylized the header component

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
 
 export default function Header() {
   return (
@@ -21,10 +27,19 @@ export default function Header() {
           />
           <SearchIcon
             className="hidden md:inline-flex h-8 bg-red-400 
-          text-white rounded-full p-2 cursor-pointer md:mx-2"
+          text-white rounded-full p-2 cursor-pointer md:mx-2 hover:bg-red-500 transition-all"
           />
         </div>
-        <div>right</div>
+        <div className="flex space-x-4 items-center justify-end">
+          <p className="hidden md:inline cursor-pointer hover:text-gray-500 transition-all">
+            Become A Host
+          </p>
+          <GlobeAltIcon className="h-6 text-gray-500 hover:text-gray-700 transition-all cursor-pointer" />
+          <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+            <MenuIcon className="h-6 text-gray-500 cursor-pointer hover:text-gray-700 transition-all" />
+            <UserCircleIcon className="h-6 text-gray-500 cursor-pointer hover:text-gray-700 transition-all" />
+          </div>
+        </div>
       </header>
     </div>
   );
